adb6f7f38bbf4f18ec8612286d491c4e08cdf7f5
Paulo Matias <matias@ufscar.br>
2022-06-21 13:40:12 -0300

    import modem

--- autograde.py
+++ autograde.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import json
+from modem import Modem
 
+def test_fsk_tx():
+    modem = Modem(48000, 48000//300, ans=False)
+    return 0
 
 def main():
     scores = {}
+    scores['fsk-tx'] = 2*test_fsk_tx()
     print(json.dumps({'scores':scores}))
 
 
