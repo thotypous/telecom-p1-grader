6dabd0b581c1c8cddfc1b16310138cc02e786f14
Paulo Matias <matias@ufscar.br>
2023-06-02 16:00:44 -0300

    fix channel model

--- channel.cpp
+++ channel.cpp
@@ -2,12 +2,13 @@
 #include <math.h>
 #include "mlinterp.hpp"
 #include "channel.hpp"
+#include "config.hpp"
 
 
-void channel_model(float noise_avg_amplitude, float timing_offset, float *samples, int n) {
+void channel_model(float noise_amplitude, float timing_offset, float *samples, int n) {
     std::random_device rd {};
     std::mt19937 gen {rd()};
-    std::normal_distribution<> d {0, sqrt(noise_avg_amplitude)};
+    std::normal_distribution<> d {0, noise_amplitude};
 
     float *noisy = new float[n];
     for (int i = 0; i < n; i++) {
@@ -35,7 +36,7 @@ void channel_model(float noise_avg_amplitude, float timing_offset, float *sample
     delete [] xi;
 }
 
-float signal_avg_watts(float *samples, int n) {
+float signal_avg_power(float *samples, int n) {
     float res = 0.;
     for (int i = 0; i < n; i++) {
         res += (samples[i]*samples[i])/n;
@@ -43,9 +44,14 @@ float signal_avg_watts(float *samples, int n) {
     return res;
 }
 
-void channel_model_snr_db(float target_snr_db, float timing_offset, float *samples, int n) {
-    float sig_avg_db = 10. * log10(signal_avg_watts(samples, n));
-    float noise_avg_db = sig_avg_db - target_snr_db;
-    float noise_avg_watts = pow(10., noise_avg_db/10.);
-    channel_model(sqrt(noise_avg_watts), timing_offset, samples, n);
+void channel_model_EbN0_dB(float EbN0_dB, float timing_offset, float *samples, int n) {
+    // see https://www.mathworks.com/help/comm/ug/awgn-channel.html
+    // in our case, Eb == Es, since we have one bit per symbol
+    const float SNR_dB = EbN0_dB - 10.*log10((float)SAMPLES_PER_SYMBOL);
+
+    const float S_dB = 10. * log10(signal_avg_power(samples, n));
+    const float N_dB = S_dB - SNR_dB;
+    const float N = pow(10., N_dB/10.);
+
+    channel_model(sqrt(N), timing_offset, samples, n);
 }
--- channel.hpp
+++ channel.hpp
@@ -2,7 +2,7 @@
 #define CHANNEL_HPP
 
 void channel_model(float noise_avg_amplitude, float timing_offset, float *samples, int n);
-float signal_avg_watts(float *samples, int n);
-void channel_model_snr_db(float target_snr_db, float timing_offset, float *samples, int n);
+float signal_avg_power(float *samples, int n);
+void channel_model_EbN0_dB(float EbN0_dB, float timing_offset, float *samples, int n);
 
 #endif
